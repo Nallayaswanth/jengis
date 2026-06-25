31cf3215e3b2aba37424bf25872e2525932b4114
NALLA YASWANTH SAI KUMAR VARMA <yaswanthnalla95@gmail.com>
2026-06-25 09:43:17 +0530

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-class Fact{
+class Fact {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int f=1;
-        for(int i = 1; i <= n; i ++){
-            System.out.println(f);
+
+        int f = 1;
+        for(int i = 1; i <= n; i++) {
             f = f * i;
         }
-    }
 
+        System.out.println("Factorial = " + f);
+    }
 }
